9a89403aaa3c4996793071bc558b78e13f9004b5
Winston McGriff <winstonmcgriff27@gmail.com>
2021-02-13 08:32:50 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,5 +1,19 @@
 def second_challenge
   epic_tragedy = {
+   :montague => {
+ :patriarch => {},
+  :matriarch => {},
+  :hero => {},
+:hero_friends => []
+ },
+:capulet => {
+ :patriarch => {},
+:matriarch => {},
+:heroine => {},
+      :heroine_friends => []
+   }
+}
+  epic_tragedy = {
     :montague => {
       #your key/value pairs here!
       }, 
